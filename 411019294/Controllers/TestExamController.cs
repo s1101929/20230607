01bd40a9c2288c666e1e0c6f3ec9822ca040207d
s1101929 <b7944210@gmail.com>
2023-06-07 19:51:51 +0800

    commit 1

---
+++ 411019294/Controllers/TestExamController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _411019294.Controllers
+{
+    public class TestExamController : Controller
+    {
+        // GET: TestExam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
