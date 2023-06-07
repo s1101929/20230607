9c9daeca97ab24356e27f2331d8d0f31dbc4120c
s1101929 <b7944210@gmail.com>
2023-06-07 20:09:47 +0800

    click me

---
+++ test1/test1/Controllers/TestExamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Controllers
+{
+    public class TestExamController : Controller
+    {
+        // GET: TestExam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
